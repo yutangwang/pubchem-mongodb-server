246c7c7d0285e94043a1288b88a7bc2554d59b88
yutangwang <kingyutang@gmail.com>
2016-06-21 21:53:39 -0400

    version 1.1

--- chemspider_crawler_json.py
+++ chemspider_crawler_json.py
@@ -1,28 +1,29 @@
 """
-Script to build mongodb from Chemspider
+Script to build json file from Chemspider
 
 """
-
-from pymongo import MongoClient
+import json
 from chemspipy import ChemSpider
 import random
 from time import gmtime, strftime, sleep
 
-client = MongoClient('localhost', 27017)
-db = client.mydb
-chemspider = db.chemspider
+cs = ChemSpider('6a4cb931-b018-4ffe-96e4-85f704e5f2a6')
 
-cs = ChemSpider('0064dc77-e5cb-4e86-93da-e8aedd62baa0')
+contributor = input("Enter your name: ")
+x = int(input("Enter the first csid to start: "))
+y = int(input("Enter the second csid to end: "))
 
-csids = list(range(30001, 40001))
+csids = list(range(x, y))
 random.shuffle(csids)
+
+doc = {'Contributor': str(contributor)}
+dt = strftime("%Y-%m-%d\t%H:%M:%S" , gmtime())
+doc['date'] = dt
+with open('chemspiderdb.json', 'w') as jsonfile:
+    json.dump(doc, jsonfile)
 doc = {}
 
 for csid in csids:
-    se = chemspider.find_one({'_id': csid})
-    if se is not None:
-        print('{0} has in the mongoDB'.format(str(csid)))
-        continue
     compound = cs.get_compound(csid)
     try:
         doc['_id'] = int(compound.csid)
@@ -36,15 +37,11 @@ for csid in csids:
         doc['stdinchikey'] = compound.stdinchikey
         doc['smiles'] = compound.smiles
         sleep(random.uniform(1, 1.2))
+        print("{0}, Thank you very much!".format(str(contributor)))
+        with open('chemspiderdb.json', 'a') as jsonfile:
+            json.dump(doc, jsonfile)
     except Exception as e:
         print(str(e) + 'Invalid ID  is ' + str(compound.csid))
         with open('Invalid_ID.txt', 'a') as invalid_id:
             invalid_id.write(str(compound.csid) + '\n')
         continue
-    try:
-        chemspider.insert_one(doc)
-        print('{0} has been inserted'.format(str(compound.csid)))
-    except Exception as e:
-        print(str(e) + "data insert error" + str(csid)+ '\n')
-        with open('mongodb_server_log.txt', 'a') as mongo_error_log:
-            mongo_error_log.write(strftime("%Y-%m-%d\t%H:%M:%S" + "---cisd " + str(doc['_id']) + 'insert error \n', gmtime()))
