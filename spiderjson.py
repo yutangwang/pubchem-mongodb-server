98bc246d782eb1b8a0eda4e8f54e91a33532a62f
yutangwang <kingyutang@gmail.com>
2016-06-23 16:04:22 -0400

    add a token of chemspider

--- spiderjson.py
+++ spiderjson.py
@@ -21,7 +21,8 @@ else:
 
 def tokenchoice():
     cs_security_key = ['6a4cb931-b018-4ffe-96e4-85f704e5f2a6', '0064dc77-e5cb-4e86-93da-e8aedd62baa0',
-                       '66aed41a-8c86-46ac-a75b-8c36db733768', '704484fb-3aa5-45f1-b6e1-faaf8aba47af']
+                       '66aed41a-8c86-46ac-a75b-8c36db733768', '704484fb-3aa5-45f1-b6e1-faaf8aba47af',
+                       'f6b8adf6-c470-4b69-9a48-41ede6572a57']
     return random.choice(cs_security_key)
 
 
@@ -37,7 +38,7 @@ if os.path.isfile('chemspiderdb.json'):
             spiderjsonfileid.append(the_dict['_id'])
             # print(spiderjsonfileid)
     for csid in csids:
-        #cskey = random.choice(cs_security_key)
+        # cskey = random.choice(cs_security_key)
         cs = ChemSpider(tokenchoice())
         if csid in spiderjsonfileid:
             print('{0} has been in the file'.format(str(csid)))
@@ -65,7 +66,7 @@ if os.path.isfile('chemspiderdb.json'):
             continue
 else:
     for csid in csids:
-        #cskey = random.choice(cs_security_key)
+        # cskey = random.choice(cs_security_key)
         cs = ChemSpider(tokenchoice())
         compound = cs.get_compound(csid)
         try:
@@ -90,4 +91,4 @@ else:
             continue
 timerstop = timeit.default_timer()
 print("Please email 'idrange.json', 'chemspiderdb.json' and 'Invalid_ID' to wangyt@neau.edu.cn")
-print("Your computer and you have worked for " + str((timerstop-timerstart)/3600) + " hours!")
+print("Your computer and you have worked for " + str((timerstop - timerstart) / 3600) + " hours!")
